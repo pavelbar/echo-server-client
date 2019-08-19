d83d00ad646b7f95001f2a5979b60176c6387792
Baranov Pavel <pavelbaranow@gmail.com>
2019-08-19 20:40:46 +0300

    Create tr.h

---
+++ make/list/tr.h
@@ -0,0 +1,33 @@
+#ifndef __TRANSPORT_H__
+#define __TRANSPORT_H__
+
+#include <sys/select.h> // fd_set
+#include "transport.h"
+
+/*Message read buffer size and clients arr size*/
+static const int BUF_SIZE = 100;
+
+/*The protocol family which will be used for communicatio*/
+static const int PROTOCOL = 0;
+
+/*The backlog argument defines the maximum length to which the queue ofpending
+ * connections for sockfd may grow*/
+static const int BACKLOG = 10;
+
+/*Setting the Timeout Value*/
+static const int TIMEOUT = 10;
+
+static int _listener = 0;  // fd
+struct TNode *_pFirst;
+struct TNode *_pCurr; 
+static fd_set _readset;
+
+void createServer(int port);
+static void createListener(int port);
+int waitingForConnection();
+int newConnection();
+int parseDateFromClient();
+void deleteServer();
+int _max(int a, int b);
+
+#endif
