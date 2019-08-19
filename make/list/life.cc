f9ab2cf2bb6648be8ab870ed369d76684639edbd
Baranov Pavel <pavelbaranow@gmail.com>
2019-08-19 20:44:51 +0300

    Rename life.c to life.cc

---
+++ make/list/life.cc
@@ -0,0 +1,29 @@
+#include "lifecycle.h"
+#include "transport.h"
+
+void lifeCycle(int port){
+  createServer(port);  		   //"transport.h"
+  int err = 0;
+  
+  while (1) {
+    err = waitingForConnection();  //"transport.h"
+    if (err == 1) {
+      // timeout
+      continue;
+    }
+
+    err = newConnection(); 	   //"transport.h"
+    if (err == 1) {
+      // fd big size
+      continue;
+    }
+
+    err = parseDateFromClient();   //"transport.h"
+    if (err == 1) {
+      // disconect
+      continue;
+    }
+  }
+
+  deleteServer();  		   //"transport.h"
+  }
